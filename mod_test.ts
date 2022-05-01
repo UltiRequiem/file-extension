ea2ce77d4f2ee8579b40d696d7b4d0706dbc6074
GitHub <noreply@github.com>
2022-05-01 00:00:29 +0000

    :arrow_up: update deno dependencies

--- mod_test.ts
+++ mod_test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from "https://deno.land/std@0.109.0/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.137.0/testing/asserts.ts";
 
 import { fileExtension } from "./mod.ts";
 
