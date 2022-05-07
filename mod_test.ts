909b24d7fe95996a2523d1774eb99e52cb2dfde0
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-05-07 10:14:43 -0500

    :white_check_mark: Add tests

--- mod_test.ts
+++ mod_test.ts
@@ -1,11 +1,20 @@
-import { assertEquals } from "https://deno.land/std@0.137.0/testing/asserts.ts";
+import {
+	assertEquals,
+	assertThrows,
+} from "https://deno.land/std@0.137.0/testing/asserts.ts";
 
 import { extname } from "./mod.ts";
 
 Deno.test("Test Default Exports", () => {
-  assertEquals(extname("test.py"), ".py");
+	assertEquals(extname("test.py"), ".py");
 
-  assertEquals(extname({ path: "mod.ts", leadingPeriod: false }), "ts");
+	assertEquals(extname({ path: "mod.ts", leadingPeriod: false }), "ts");
 
-  assertEquals(extname({ path: "App.js", leadingPeriod: true }), ".js");
+	assertEquals(extname({ path: "App.js", leadingPeriod: true }), ".js");
+
+	assertThrows(() => {
+		extname({ path: "App", leadingPeriod: false });
+	});
+
+	assertEquals(extname({ path: "data.gz.arg", leadingPeriod: true }), ".arg");
 });
