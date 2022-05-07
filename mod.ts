513a0ea30a833e032483bb8b4ecaade2df1b6192
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-05-07 09:47:48 -0500

    :recycle: Update

--- mod.ts
+++ mod.ts
@@ -1,68 +1,77 @@
-// Ported from https://github.com/browserify/path-browserify
-// Works on Deno, Node.js and the browser.
-// Only Deno?: https://deno.land/std/path#extname
-// Only Node.js?: https://nodejs.org/api/path.html#pathextnamepath
+/**
+ * This module is browser compatible.
+ * Ported from https://github.com/browserify/path-browserify
+ * @module
+ */
 
 /**
  * Return the extension of the "file" with leading period.
+ *
+ * Works on Deno, Node.js and the browser.
+ *
+ * Only Deno? {@link https://deno.land/std/path#extname}
+ * Only Node.js? {@link https://nodejs.org/api/path.html#pathextnamepath}
+ *
  * @param file with extension.
  * @returns extension (ex. for "main.py" returns ".py")
  */
-export function extname(file: string) {
-  let startDot = -1;
-  let startPart = 0;
-  let end = -1;
-  let matchedSlash = true;
+export function unixExtname(file: string) {
+	let startDot = -1;
+	let startPart = 0;
+	let end = -1;
+	let matchedSlash = true;
 
-  // Track the state of characters (if any) we see before our first dot and
-  // after any path separator we find
-  let preDotState = 0;
+	// Track the state of characters (if any) we see before our first dot and
+	// after any path separator we find
+	let preDotState = 0;
 
-  for (let i = file.length - 1; i >= 0; --i) {
-    const code = file.charCodeAt(i);
+	for (let i = file.length - 1; i >= 0; --i) {
+		const code = file.charCodeAt(i);
 
-    if (code === 47) {
-      // If we reached a path separator that was not part of a set of path
-      // separators at the end of the string, stop now
-      if (!matchedSlash) {
-        startPart = i + 1;
-        break;
-      }
+		if (code === 47) {
+			// If we reached a path separator that was not part of a set of path
+			// separators at the end of the string, stop now
+			if (!matchedSlash) {
+				startPart = i + 1;
+				break;
+			}
 
-      continue;
-    }
+			continue;
+		}
 
-    if (end === -1) {
-      // We saw the first non-path separator, mark this as the end of our
-      // extension
-      matchedSlash = false;
-      end = i + 1;
-    }
+		if (end === -1) {
+			// We saw the first non-path separator, mark this as the end of our
+			// extension
+			matchedSlash = false;
+			end = i + 1;
+		}
 
-    if (code === 46 /*.*/) {
-      // If this is our first dot, mark it as the start of our extension
-      if (startDot === -1) {
-        startDot = i;
-      } else if (preDotState !== 1) {
-        preDotState = 1;
-      }
-    } else if (startDot !== -1) {
-      // We saw a non-dot and non-path separator before our dot, so we should
-      // have a good chance at having a non-empty extension
-      preDotState = -1;
-    }
-  }
+		if (code === 46 /*.*/) {
+			// If this is our first dot, mark it as the start of our extension
+			if (startDot === -1) {
+				startDot = i;
+			} else if (preDotState !== 1) {
+				preDotState = 1;
+			}
+		} else if (startDot !== -1) {
+			// We saw a non-dot and non-path separator before our dot, so we should
+			// have a good chance at having a non-empty extension
+			preDotState = -1;
+		}
+	}
 
-  if (
-    startDot === -1 ||
-    end === -1 ||
-    // We saw a non-dot character immediately before the dot
-    preDotState === 0 ||
-    // The (right-most) trimmed path component is exactly '..'
-    (preDotState === 1 && startDot === end - 1 && startDot === startPart + 1)
-  ) {
-    throw new Error(`Path ${file} does not have a valid extension.`);
-  }
+	if (
+		startDot === -1 ||
+		end === -1 ||
+		// We saw a non-dot character immediately before the dot
+		preDotState === 0 ||
+		// The (right-most) trimmed path component is exactly '..'
+		(preDotState === 1 && startDot === end - 1 && startDot === startPart + 1)
+	) {
+		throw new Error(`Path ${file} does not have a valid extension.`);
+	}
 
-  return file.slice(startDot, end);
+	return file.slice(startDot, end);
 }
+
+export const extname = unixExtname;
